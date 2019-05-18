8d2d20999f812cd1db8cdcd3390ab67b2401a772
AlexStephan <amoldovan@itba.edu.ar>
2019-05-18 19:28:34 -0300

    Subo un header general. Seria recomendable q todos lo utilizaramos, asi es mas facil comunicarse entre las funciones (para avisar verdadero/falso, o error/sin error)

---
+++ Calculadora/general.h
@@ -0,0 +1,23 @@
+
+/* 
+ * File:   general.h
+ * Author: tefi
+ *
+ * Created on May 18, 2019, 7:25 PM
+ */
+
+/*
+ * Este header cuenta con definiciones útiles para la comunicación entre las funciones
+ */
+
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#define ERROR 1
+#define SIN_ERROR 0
+
+#define VERDAD 1
+#define FALSO 0
+
+#endif /* GENERAL_H */
+
