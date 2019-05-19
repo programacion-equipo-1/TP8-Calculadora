6376e5a1336565c2fd459aebd8bf3201f70c388c
AlexStephan <amoldovan@itba.edu.ar>
2019-05-18 23:38:13 -0300

    Termine el Main

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -15,7 +15,12 @@
 #define agregar(simbolo,funcion) (error = error || add_op(simbolo,funcion))
 
 int main(int argc, char* argv[]) {
+    
+    char** argumento = argv+1;
     int error = FALSO;
+    float resultado;
+    
+    
     agregar('+',&suma);
     agregar('-',&resta);
     agregar('*',&multiplicacion);
@@ -25,9 +30,14 @@ int main(int argc, char* argv[]) {
         return 0;
     }
     
+    error = calculador_recursivo(&argumento,&resultado);
+    if (error == VERDAD){
+        printf("Main: ERROR DE OPERACIÓN - Programa abortado\n");
+        return 0;
+    }
     
-    
-    
+    printf("RESULTADO:\n\t%f\n",resultado);
+    printf("Que tenga un maravilloso día ♡ ♡ ♡\n");
     return 0;
 }
 
